6755f5b514d92ccde65c87c9f75d709215df43bc
Sashko Sh <sashkoace@gmail.com>
2023-04-16 21:40:31 +0300

    added fev sections

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -6,7 +6,6 @@ import { flsModules } from './modules.js';
 const cross = document.querySelector('.header__info-cross'),
   info = document.querySelector('.header__info');
 
-console.log(cross);
 cross.addEventListener('click', () => {
   info.classList.add('closed');
 });
